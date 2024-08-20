b846ad281c39e4d6646bb31f7bd74ab369299efe
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2024-08-20 22:55:57 +0700

    java docs

--- src/main/java/io/hoangtien2k3/commons/factory/ModelMapperFactory.java
+++ src/main/java/io/hoangtien2k3/commons/factory/ModelMapperFactory.java
@@ -17,7 +17,7 @@ package io.hoangtien2k3.commons.factory;
 import org.modelmapper.ModelMapper;
 
 public class ModelMapperFactory {
-  private static ModelMapper modelMapper = new ModelMapper();
+  private static final ModelMapper modelMapper = new ModelMapper();
 
   public static ModelMapper getInstance() {
     return modelMapper;
